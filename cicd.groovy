e47a42f6bdc2c59603119a30a36fb79ba28f205d
Igor Todorovski <itodorov@ca.ibm.com>
2024-09-20 17:20:42 -0400

    Change organization to zopencommunity

--- cicd.groovy
+++ cicd.groovy
@@ -6,10 +6,10 @@ node('linux')
                         branches: [[name: '*/main']],
                         doGenerateSubmoduleConfigurations: false,
                         extensions: [],
-                        userRemoteConfigs: [[url: 'https://github.com/ZOSOpenTools/zstdport.git']]])
+                        userRemoteConfigs: [[url: 'https://github.com/zopencommunity/zstdport.git']]])
         }
 
         stage('Build') {
-                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/ZOSOpenTools/zstdport.git'), string(name: 'PORT_DESCRIPTION', value: 'A fast lossless compression algorithm, targeting real-time compression scenarios at zlib-level' )]
+                build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/zstdport.git'), string(name: 'PORT_DESCRIPTION', value: 'A fast lossless compression algorithm, targeting real-time compression scenarios at zlib-level' )]
         }
 }
